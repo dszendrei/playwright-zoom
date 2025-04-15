658ace51a149631a370ae2674f1880e997b58716
dszendrei <r.daniel.szendrei@gmail.com>
2025-04-15 21:39:05 +0200

    Add option to use custom extensions

--- README.md
+++ README.md
@@ -10,12 +10,13 @@
 - Uses the **Playwright Zoom Extension** to ensure accurate zoom functionality across supported browsers.
 - Fully compatible with modern Node.js and TypeScript projects.
 - Supports adjustable zoom levels for testing responsive designs and accessibility.
+- Load custom browser extensions alongside the Playwright Zoom Extension for additional flexibility.
 
 ---
 
 ## Limitations
 
-- only works with Chromium, since it is using a Chrome extension.
+- Only works with Chromium, since it relies on a Chrome extension.
 
 ---
 
@@ -47,8 +48,36 @@ testWithZoomExtension(
 );
 ```
 
-I would recommend this trick to make it nicer to use:
+I recommend this trick to make the syntax simpler:
 
 ```typescript
 import { setBrowserZoom, testWithZoomExtension as test } from "playwright-zoom";
+```
+
+## Custom Extensions
+
+Playwright Zoom also supports loading custom browser extensions alongside the Playwright Zoom Extension. This allows you to integrate additional extensions into your Playwright tests. (Since 1.1.0)
+
+### Configuring Custom Extensions
+
+You can specify the paths to custom extensions in the Playwright configuration file. Use the pathsToCustomExtensions option, which is part of the PathsToCustomExtensions type provided by the library:
+
+```typescript
+import { defineConfig, devices } from "@playwright/test";
+import type { PathsToCustomExtensions } from "playwright-zoom";
+
+export default defineConfig<PathsToCustomExtensions>({
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        pathsToCustomExtensions: [
+          "/absolute/path/to/custom-extension-1",
+          "/absolute/path/to/custom-extension-2",
+        ],
+      },
+    },
+  ],
+});
 ```
--- src/lib/pw-zoom.ts
+++ src/lib/pw-zoom.ts
@@ -1,13 +1,67 @@
 import { test, chromium, Page } from "@playwright/test";
 
-export const testWithZoomExtension = test.extend({
-  context: async ({ context }, use) => {
-    const pathToExtension = require("path").join(__dirname, "/zoom-extension");
+export type PathsToCustomExtensions = {
+  /**
+   * Optional array of absolute paths to custom browser extensions.
+   */
+  pathsToCustomExtensions?: string[];
+};
+
+/**
+ * A Playwright test extension that integrates the Playwright Zoom Extension into the browser context.
+ * This allows tests to set and manipulate browser zoom levels to replicate real user experience.
+ *
+ * The custom test type extends Playwright's behavior by launching a persistent Chromium context
+ * with the required browser extension preloaded. It ensures the test context is properly initialized
+ * with cookies and manages cleanup tasks after each test run.
+ *
+ * Additionally, users can load custom browser extensions by specifying their absolute paths in the Playwright configuration.
+ *
+ * @example Basic Zoom Functionality
+ * import { setBrowserZoom, testWithZoomExtension as test } from 'playwright-zoom';
+ *
+ * test('Zoom Test Example', async ({ page }) => {
+ *   // Set the browser zoom level
+ *   await setBrowserZoom(page, 200);
+ *   console.log('Zoom level set to 200%');
+ * });
+ *
+ * @example Configuring Custom Extensions
+ * import { defineConfig, devices } from '@playwright/test';
+ * import type { PathsToCustomExtensions } from 'playwright-zoom';
+ *
+ * export default defineConfig<PathsToCustomExtensions>({
+ *   projects: [
+ *     {
+ *       name: 'chromium',
+ *       use: {
+ *         ...devices['Desktop Chrome'],
+ *         pathsToCustomExtensions: [
+ *           '/absolute/path/to/custom-extension-1',
+ *           '/absolute/path/to/custom-extension-2'
+ *         ],
+ *       },
+ *     },
+ *   ],
+ * });
+ *
+ * @extends {import('@playwright/test').TestType}
+ */
+export const testWithZoomExtension = test.extend<PathsToCustomExtensions>({
+  pathsToCustomExtensions: [undefined, { option: true }],
+  context: async ({ context, pathsToCustomExtensions }, use) => {
+    const pathToZoomExtension = require("path").join(
+      __dirname,
+      "/zoom-extension"
+    );
+    const joinedPaths = pathsToCustomExtensions
+      ? `${pathToZoomExtension},${pathsToCustomExtensions.join()}`
+      : pathToZoomExtension;
     const contextWithExtension = await chromium.launchPersistentContext("", {
       headless: false,
       args: [
-        `--disable-extensions-except=${pathToExtension}`,
-        `--load-extension=${pathToExtension}`,
+        `--disable-extensions-except=${joinedPaths}`,
+        `--load-extension=${joinedPaths}`,
       ],
     });
     await contextWithExtension.addCookies(await context.cookies());
@@ -15,11 +69,14 @@ export const testWithZoomExtension = test.extend({
       let pageWithExntesionToClose: Page | undefined =
         contextWithExtension.pages()[0];
       contextWithExtension.on("page", async () => {
-        await pageWithExntesionToClose?.close();
+        await pageWithExntesionToClose?.close(); // the extension opens a tab upon load
         pageWithExntesionToClose = undefined;
       });
     } catch (error) {
-      console.error("Could not close the extension loading first tab:", error);
+      console.error(
+        "Failed to close the first tab after initializing the extension:",
+        error
+      );
     }
     await use(contextWithExtension);
     try {
@@ -31,9 +88,12 @@ export const testWithZoomExtension = test.extend({
 });
 
 /**
- * Setting the browser zoom is only available in Chrome. To set the zoom the Playwright Zoom Extension has to be loaded in the test browser by using the testWithZoomExtension.
+ * Sets the browser zoom in a Chrome instance using the Playwright Zoom Extension.
  *
- * @param zoom Browser zoom to set in Chrome
+ * Note: This function only works if the browser is launched with the `testWithZoomExtension`.
+ *
+ * @param page The Playwright Page object.
+ * @param zoom The zoom level to set (e.g., 150 for 150%).
  */
 export async function setBrowserZoom(page: Page, zoom: number) {
   await page.evaluate(
